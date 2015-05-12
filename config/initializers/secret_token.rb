ae3583c3c0a94b1c9dc75adf3305d266b7b6ec1c
Gustavo Saiani <gustavo@poe.ma>
2015-05-12 10:21:39 -0300

    rails new react-rails-tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReactRailsTutorial::Application.config.secret_key_base = 'ce8c0aa8716e80aa4e28a87d7a49fff0bfe63410dcb376e3cbf4f9b4ad71977018f579e1f3ce09a3d96dd8a79ffe128b8bae77db929f64dd562ae1826eb82c27'
